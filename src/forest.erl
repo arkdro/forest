e6fa24914ff7669f05c6b47e850438ed2ccc0d79
Arkadiy Drovosekov <arkdro@gmail.com>
2016-02-06 18:35:22 +0300

    add build_forest/2

--- src/forest.erl
+++ src/forest.erl
@@ -1,8 +1,15 @@
 -module(forest).
 
 -export([
+         build_forest/2,
          count_trees/1
         ]).
+
+build_forest(Vs, Es) ->
+    G = digraph:new(),
+    [digraph:add_vertex(G, V) || V <- Vs],
+    [digraph:add_edge(G, E, V1, V2, []) || {V1, V2} = E <- Es],
+    G.
 
 count_trees(G) ->
     Components = digraph_utils:components(G),
