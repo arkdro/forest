e580d4b3b8d9c492c3edc710bd4be1870c08ec9c
Arkadiy Drovosekov <arkdro@gmail.com>
2016-02-06 18:35:11 +0300

    add count_trees/1

--- src/forest.erl
+++ src/forest.erl
@@ -1,7 +1,14 @@
 -module(forest).
 
 -export([
+         count_trees/1
         ]).
+
+count_trees(G) ->
+    Components = digraph_utils:components(G),
+    L = [digraph_utils:subgraph(G, X) || X <- Components],
+    L2 = [1 || X <- L, digraph_utils:is_tree(X)],
+    length(L2).
 
 %% ===================================================================
 %% Internal functions
